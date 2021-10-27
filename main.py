ad217ac33e11f9a602841aaab8e08c635d4ca248
bryanwaff <bryanwaff5@gmail.com>
2021-10-27 10:00:05 +0300

    Update

--- main.py
+++ main.py
@@ -7,11 +7,11 @@ root = Tk()
 root.title("Bryan waff - Clock")
 
 def time():
-    string = strftime('%h %y - %H:%M:%S')
+    string = strftime(' %H:%M:%S - %h %y ')
     label.config(text=string)
     label.after(1000, time)
 
-label = Label(root, font=("ds-digital", 80), background = "black", foreground= "cyan")
+label = Label(root, font=("ds-digital", 30), background = "black", foreground= "cyan")
 label.pack(anchor='center')
 time()
 
